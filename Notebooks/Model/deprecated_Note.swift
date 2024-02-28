b7ac81cb4ce6bd6ffc81ac98a0f41ae81f9b7dca
Maltine Rama <94307059+MaltineR@users.noreply.github.com>
2024-02-28 21:33:25 +0100

    Update deprecated_Note.swift

--- Notebooks/Model/deprecated_Note.swift
+++ Notebooks/Model/deprecated_Note.swift
@@ -1,1 +1,21 @@
+import Foundation
 
+struct deprecated_Note {
+    let title: String
+    let text: String?
+    let tags: [String]?
+    let creationDate: Date
+    let lastSeenDate: Date?
+   
+    
+    static let dummyNotesModel: [deprecated_Note] = [
+        deprecated_Note(title: "First Note ", text: nil, tags: nil, creationDate: Date(), lastSeenDate: nil),
+        deprecated_Note(title: "Second Note", text: "Test", tags: nil, creationDate: Date(), lastSeenDate: nil),
+        deprecated_Note(title: "Third Note", text: "TextNote", tags: nil, creationDate: Date(), lastSeenDate: nil),
+        deprecated_Note(title: "Fourth Note", text: "Some Content", tags: nil, creationDate: Date(), lastSeenDate: nil),
+        deprecated_Note(title: "Fifth Note ", text: nil, tags: nil, creationDate: Date(), lastSeenDate: nil)
+    ]
+
+    
+
+}
