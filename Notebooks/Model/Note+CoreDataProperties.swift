ed9d653402b59137b2d1101c981a92a5d41e29a8
Maltine Rama <94307059+MaltineR@users.noreply.github.com>
2024-02-28 21:29:47 +0100

    Update Note+CoreDataProperties.swift

--- Notebooks/Model/Note+CoreDataProperties.swift
+++ Notebooks/Model/Note+CoreDataProperties.swift
@@ -1,1 +1,18 @@
+import Foundation
+import CoreData
 
+
+extension Note {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Note> {
+        return NSFetchRequest<Note>(entityName: "Note")
+    }
+
+    @NSManaged public var creationDate: NSDate?
+    @NSManaged public var lastSeenDate: NSDate?
+    @NSManaged public var text: String?
+    @NSManaged public var title: String?
+    @NSManaged public var image: NSData?
+    @NSManaged public var notebook: Notebook?
+
+}
