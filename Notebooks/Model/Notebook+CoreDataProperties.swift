6fd9616c1c2be31935cc33634b7c52267d11b10c
Maltine Rama <94307059+MaltineR@users.noreply.github.com>
2024-02-28 21:30:57 +0100

    Update Notebook+CoreDataProperties.swift

--- Notebooks/Model/Notebook+CoreDataProperties.swift
+++ Notebooks/Model/Notebook+CoreDataProperties.swift
@@ -1,18 +1,53 @@
+
+
 import Foundation
 import CoreData
 
 
-extension Note {
+extension Notebook {
 
-    @nonobjc public class func fetchRequest() -> NSFetchRequest<Note> {
-        return NSFetchRequest<Note>(entityName: "Note")
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Notebook> {
+        return NSFetchRequest<Notebook>(entityName: "Notebook")
     }
 
     @NSManaged public var creationDate: NSDate?
-    @NSManaged public var lastSeenDate: NSDate?
-    @NSManaged public var text: String?
-    @NSManaged public var title: String?
-    @NSManaged public var image: NSData?
-    @NSManaged public var notebook: Notebook?
+    @NSManaged public var name: String?
+    @NSManaged public var notes: NSOrderedSet?
 
 }
+
+// MARK: Generated accessors for notes
+extension Notebook {
+
+    @objc(insertObject:inNotesAtIndex:)
+    @NSManaged public func insertIntoNotes(_ value: Note, at idx: Int)
+
+    @objc(removeObjectFromNotesAtIndex:)
+    @NSManaged public func removeFromNotes(at idx: Int)
+
+    @objc(insertNotes:atIndexes:)
+    @NSManaged public func insertIntoNotes(_ values: [Note], at indexes: NSIndexSet)
+
+    @objc(removeNotesAtIndexes:)
+    @NSManaged public func removeFromNotes(at indexes: NSIndexSet)
+
+    @objc(replaceObjectInNotesAtIndex:withObject:)
+    @NSManaged public func replaceNotes(at idx: Int, with value: Note)
+
+    @objc(replaceNotesAtIndexes:withNotes:)
+    @NSManaged public func replaceNotes(at indexes: NSIndexSet, with values: [Note])
+
+    @objc(addNotesObject:)
+    @NSManaged public func addToNotes(_ value: Note)
+
+    @objc(removeNotesObject:)
+    @NSManaged public func removeFromNotes(_ value: Note)
+
+    @objc(addNotes:)
+    @NSManaged public func addToNotes(_ values: NSOrderedSet)
+
+    @objc(removeNotes:)
+    @NSManaged public func removeFromNotes(_ values: NSOrderedSet)
+
+}
+
