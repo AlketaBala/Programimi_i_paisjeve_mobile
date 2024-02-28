501a4258e3de6ce27e5d999a3cd5dce365d1f44a
Maltine Rama <94307059+MaltineR@users.noreply.github.com>
2024-02-28 21:24:36 +0100

    Update Note+CoreDataClass.swift

--- Notebooks/Model/Note+CoreDataClass.swift
+++ Notebooks/Model/Note+CoreDataClass.swift
@@ -1,1 +1,21 @@
 
+import Foundation
+import CoreData
+
+@objc(Note)
+public class Note: NSManagedObject {
+
+    
+}
+
+extension Note {
+    
+    func csv() -> String {
+        let exportedTitle = title ?? "UnTitled"
+        let exportedText = text ?? ""
+        let exportedCreationDate = (creationDate as Date?)?.customStringLabel() ?? "ND"
+        
+        return "\(exportedCreationDate),\(exportedTitle),\(exportedText)"
+    }
+}
+
