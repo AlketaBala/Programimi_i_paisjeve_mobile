f1466d042500f917d72074850b333ef2a5bbf155
AlketaBala <93828279+AlketaBala@users.noreply.github.com>
2024-02-28 21:29:14 +0100

    Update NotebookListCell.swift

--- Notebooks/NotebookList/NotebookListCell.swift
+++ Notebooks/NotebookList/NotebookListCell.swift
@@ -1,1 +1,18 @@
+import UIKit
 
+class NotebookListCell: UITableViewCell {
+    
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var creationDateLabel: UILabel!
+    
+    override func prepareForReuse() {
+        titleLabel.text = nil
+        creationDateLabel.text = nil
+        super.prepareForReuse()
+    }
+    
+    func configure(with notebook: Notebook) {
+        titleLabel.text = notebook.name
+        creationDateLabel.text = "Created: \((notebook.creationDate as Date?)?.customStringLabel() ?? "ND")"
+    }
+}
