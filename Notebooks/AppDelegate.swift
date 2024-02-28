6dbe076fd00124642a317de22707cbce0d64730b
Maltine Rama <94307059+MaltineR@users.noreply.github.com>
2024-02-28 21:45:40 +0100

    Update AppDelegate.swift

--- Notebooks/AppDelegate.swift
+++ Notebooks/AppDelegate.swift
@@ -1,1 +1,30 @@
+import UIKit
+import CoreData
 
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    lazy var coreDataStack = CoreDataStack(modelName: "EverPobre")
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        guard
+            let navController = window?.rootViewController as? UINavigationController,
+            let viewController = navController.topViewController as? NotebookListViewController
+            else { return true }
+      
+        //viewController.managedContext = coreDataStack.managedContext
+        viewController.coredataStack = coreDataStack
+        
+        return true
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        coreDataStack.saveContext()
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        coreDataStack.saveContext()
+    }
+}
