22559499dd98858453c9572513e3804499eeb225
AlketaBala <93828279+AlketaBala@users.noreply.github.com>
2024-02-28 21:40:57 +0100

    Update CoreDataStack.swift

--- Notebooks/Utils/CoreDataStack.swift
+++ Notebooks/Utils/CoreDataStack.swift
@@ -1,1 +1,42 @@
+import Foundation
+import CoreData
 
+class CoreDataStack {
+    
+    private let modelName: String
+    
+    
+    lazy var managedContext: NSManagedObjectContext = {
+        return self.storeContainer.viewContext
+    }()
+    
+    
+    lazy var backgroundContex : NSManagedObjectContext = {
+        return self.storeContainer.newBackgroundContext()
+    }()
+    
+    
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+    
+   /* private */ lazy var storeContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: self.modelName)
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error as NSError? {
+                print("Unresolved  error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    func saveContext() {
+        guard managedContext.hasChanges else { return }
+        
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Unresolved error \(error), \(error.userInfo)")
+        }
+    }
+}
